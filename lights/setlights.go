7741656560071489ae72350aa6bfabde85c82a0e
JonasFroiland <jonassfr@stud.ntnu.no>
2026-03-11 14:00:56 +0100

    finished SetLights module

--- lights/setlights.go
+++ lights/setlights.go
@@ -2,4 +2,25 @@ package setlights
 
 import "heis/elevio"
 import "heis/config"
-import "heis/distributor/commonstate"
+import "heis/distributor"
+
+
+func SetPanelLights(CommonState distributor.CommonState, ElevatorNumber int) {
+	for floor := 0; floor < config.NumFloors; floor++ {
+		for buttonType := 0; buttonType < 2; buttonType++ {
+			if CommonState.HallCalls[floor][buttonType] {
+				elevio.SetButtonLamp(elevio.ButtonType(buttonType), floor, true)
+			} else {
+				elevio.SetButtonLamp(elevio.ButtonType(buttonType), floor, false)
+			}
+		}
+	}
+	
+	for floor := 0; floor < config.NumFloors; floor++ {
+		if CommonState.Elevators[ElevatorNumber].CabCalls[floor] {
+			elevio.SetButtonLamp(elevio.BT_Cab, floor, true)
+		} else {
+			elevio.SetButtonLamp(elevio.BT_Cab, floor, false)
+		}
+	}
+}
