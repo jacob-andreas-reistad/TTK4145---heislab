f12155e42c1fb20cc11d29c8424f7d688608dc23
jacob-andreas-reistad <reistad@gmail.com>
2026-03-11 12:03:44 +0100

    implemented a motor direction module

--- elevator/elevator_doors.go
+++ elevator/elevator_doors.go
@@ -1,3 +1,18 @@
 // This file contains the FSM for the elevator system... tbc
 
 package elevator
+
+import (
+	"heis/config"
+	"heis/elevio"
+)
+
+type Orders [config.NumFloors][config.NumButtons]bool
+
+func (o Orders) has_order(dir MotorDirection, floor int) bool {
+	// Check if there is an order in the given direction at the given floor
+}
+
+func order_done(dir MotorDirection, floor int, o Orders, orderDoneCh chan<- elevio.ButtonEvent){
+	// Send an order done event to the order handler
+}
