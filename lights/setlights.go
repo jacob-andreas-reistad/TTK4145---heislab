ec77fabf226dbd62c23552d8ea8320cb3e20a8ce
JonasFroiland <jonassfr@stud.ntnu.no>
2026-03-20 12:54:21 +0100

    cleaned setlights.go

--- lights/setlights.go
+++ lights/setlights.go
@@ -6,10 +6,10 @@ import (
 	"heis/elevio"
 )
 
-func SetPanelLights(CommonState distributor.CommonState, ElevatorNumber int) {
+func SetPanelLights(commonState distributor.CommonState, elevatorNumber int) {
 	for floor := 0; floor < config.NumFloors; floor++ {
 		for buttonType := 0; buttonType < 2; buttonType++ {
-			if CommonState.HallCalls[floor][buttonType] {
+			if commonState.HallCalls[floor][buttonType] {
 				elevio.SetButtonLamp(elevio.ButtonType(buttonType), floor, true)
 			} else {
 				elevio.SetButtonLamp(elevio.ButtonType(buttonType), floor, false)
@@ -18,7 +18,7 @@ func SetPanelLights(CommonState distributor.CommonState, ElevatorNumber int) {
 	}
 
 	for floor := 0; floor < config.NumFloors; floor++ {
-		if CommonState.Elevators[ElevatorNumber].CabCalls[floor] {
+		if commonState.Elevators[elevatorNumber].CabCalls[floor] {
 			elevio.SetButtonLamp(elevio.BT_Cab, floor, true)
 		} else {
 			elevio.SetButtonLamp(elevio.BT_Cab, floor, false)
