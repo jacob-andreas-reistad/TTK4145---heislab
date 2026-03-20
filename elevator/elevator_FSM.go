0904d4f0957d94aa7136f1cd558f86785c5a76b7
jacob-andreas-reistad <reistad45@gmail.com>
2026-03-20 12:08:19 +0100

    .

--- elevator/elevator_FSM.go
+++ elevator/elevator_FSM.go
@@ -34,7 +34,7 @@ func (bh Behaviour) ToString() string {
 	case Moving:
 		return "moving"
 	case DoorsOpen:
-		return "doors open"
+		return "doorsOpen"
 	default:
 		panic("invalid behaviour")
 	}
